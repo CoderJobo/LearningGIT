0cd503667ac44d140b97d6d431d7834c73ae16bd
JoElHi <joanehickey@yahoo.ie>
2020-08-14 10:29:23 +0100

    for master

--- JoanClass.java
+++ JoanClass.java
@@ -4,5 +4,6 @@ public class JoanClass{
         System.out.println("Learning GIT adding more feature");
         System.out.println("Learning GIT adding more feature for next commit");
         System.out.println("Learning GIT adding more feature for next commit by Joan");
+        System.out.println("Learning GIT adding more feature for next commit by Joan for master push");
     }
 }
